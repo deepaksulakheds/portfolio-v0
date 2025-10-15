c4edecf63a5d7779d38ac3ba04988d0428491164
Deepak Sulakhe <deepak@zeliot.in>
2025-10-15 18:06:17 +0530

    resolved imports

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/portfolio-v0",
   plugins: [react()],
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
 });
