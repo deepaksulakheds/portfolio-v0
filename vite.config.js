29f218faa298d205ed08292698928da871299c74
Deepak Sulakhe <deepak@zeliot.in>
2025-11-21 17:11:47 +0530

    env fix in vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: import.meta.env.VITE_APP_BASE_URL,
-  plugins: [react()],
-  server: {
-    open: true,
-  },
-});
+export default ({ mode }) => {
+  // Load env variables
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return defineConfig({
+    plugins: [react()],
+    base: env.VITE_APP_BASE_URL || "/",
+    server: {
+      open: true,
+    },
+  });
+};
